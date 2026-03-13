6287080a370218951f419e5ecc3dc05da33d4de4
kve <vadim.karimov.06@gmail.com>
2026-03-13 19:45:06 +0500

    добавил отчет, исправил недочеты

--- climate_repair/requests_app/views.py
+++ climate_repair/requests_app/views.py
@@ -1,4 +1,4 @@
-from django.forms import models
+from django.db import models
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
